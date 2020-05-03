64094fdc20eecadd745ea5eca9f509ce6bc6a635
Сафонов Вадим <cska1_94@mail.ru>
2020-05-03 12:04:54 +0300

    lesson 4-4(1,3) done

--- MainString/src/Main.java
+++ MainString/src/Main.java
@@ -1,22 +1,32 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("u0041, u0042, u0043, u0044, u0045, u0046, u0047, u0048, u0049, u004A, u004B, u004C, u004D, u004E, u004F  " +
-                "u0050, u0051, u0052, u0053, u0054, u0055, u0056, u0057, u0058, u0059, u005A");
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
-        System.out.println("\u0041, \u0042, \u0043, \u0044, \u0045, \u0046, \u0047, \u0048, \u0049, \u004A, \u004B, \u004C, \u004D, \u004E, \u004F," +
-                "\u0050, \u0051, \u0052, \u0053, \u0054, \u0055, \u0056, \u0057, \u0058, \u0059, \u005A");
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
 
-        Scanner scanner = new Scanner(System.in);
+public class Main {
+
+    public void yourFio() {
         System.out.println("Введите ваше ФИО");
+        Scanner scanner = new Scanner(System.in);
         String fio = scanner.nextLine();
-        String razdel = " ";
-        String[] subStr = fio.split(razdel, 3);
+        String newFio = fio.trim();
+        fio.replaceAll(" ", " ");
+        String space = " ";
+        String[] subStr = newFio.split(space, 3);
         System.out.println("Фамилия: " + subStr[0]);
         System.out.println("Имя: " + subStr[1]);
         System.out.println("Отчество: " + subStr[2]);
+    }
 
+    public void alphaped() {
+        for (char i = 'A'; i <= 'Z'; i++) {
+            int c = i;
+            System.out.println(i + " :" + c);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.yourFio();
+        System.out.println("+++++++++++++++++++++++++++++++");
+        main.alphaped();
     }
 }
