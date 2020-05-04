eb8261e97cf95bc879eb21b21e1ac442956126be
Сафонов Вадим <cska1_94@mail.ru>
2020-05-04 16:31:08 +0300

    lesson 4-6 done

---
+++ 4-6 Time/src/Dr.java
@@ -0,0 +1,23 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class Dr {
+    public static void main(String[] args) {
+        DateFormat dateFormat = new SimpleDateFormat(" dd.MM.yyyy - EEE", Locale.ENGLISH);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1994, Calendar.AUGUST, 23);
+        Date date = new Date();
+        Date me = calendar.getTime();
+        int i = 0;
+        while (date.compareTo(me) > 0){
+            System.out.println(i + dateFormat.format(me));
+            calendar.add(Calendar.YEAR, 1);
+            me = calendar.getTime();
+            i++;
+
+        }
+    }
+}
