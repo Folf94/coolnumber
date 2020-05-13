692fc95885d43514d4c0664aaf7730eb0c6117bf
Сафонов Вадим <cska1_94@mail.ru>
2020-05-13 20:48:51 +0300

    lesson 5-4 didn't done

--- 05_ArraysAndCollections/AddressBook/src/Main.java
+++ 05_ArraysAndCollections/AddressBook/src/Main.java
@@ -1,10 +1,8 @@
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
 
-    private static final String MESSAGE = "Введите ваше имя или номер телефона";
+    private static final String MESSAGE = "Введите ваше имя.";
 
     public static void main(String[] args) {
         AddressBook.setNameOrTelephone();
@@ -14,48 +12,43 @@ public class Main {
     public static class AddressBook {
 
         public static void setNameOrTelephone() {
-           // System.out.println(MESSAGE);
-            TreeMap<String, String> addressBook = new TreeMap<>();
+            Map<String, Long> addressBook = new TreeMap<>();
+            Map<String, Integer> nameBook = new TreeMap<>();
+            Map<Long, Integer> numberBook = new TreeMap<>();
 
-            Scanner scanner = new Scanner(System.in);
             for (; ; ) {
-
+                Scanner scanner = new Scanner(System.in);
+                System.out.println(MESSAGE);
+                Long number = null;
+                String name = scanner.nextLine().trim().replaceAll("[^0-9],", "");
+                if (name.equals("LIST")) {
+                    printMap(addressBook);
                     System.out.println(MESSAGE);
-                    String name = scanner.nextLine();
-                    if (name.equals("LIST")) {
-                        printMap(addressBook);
-                        System.out.println(MESSAGE);
-                        continue;
-                    }
-                String telephonNumber = scanner.nextLine();
-                    if (addressBook.containsKey(name)) {
-                        addressBook.put(name, telephonNumber);
-                        if (name.equals(name)) {
-                            System.out.println("Данное имя уже используется, введите номер телефона");
-                            telephonNumber = scanner.nextLine();
-                            if (!telephonNumber.equals(telephonNumber)) {
-                                addressBook.put(name, telephonNumber);
-                                if (telephonNumber.equals(telephonNumber)) {
-                                    System.out.println("Ваш номер уже используется");
-                                    if (name.equals(name)) {
-                                        System.out.println("Вы уже зарегестрированы");
-                                        continue;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    addressBook.put(name, telephonNumber);
-                    //System.out.println(MESSAGE);
+                    continue;
+                }
+                nameBook.put(name, nameBook.get(name) + 1);
+                if (nameBook.containsKey(name)) {
+                    System.out.println("Введите номер телефона");
+                    number = scanner.nextLong();
+                    numberBook.put(number, numberBook.get(number) + 1);
+                }
+                if (nameBook.containsKey(name) && numberBook.containsKey(number)) {
+                    System.out.println("Вы уже зарегестрированы");
+                } else {
+                    addressBook.put(nameBook.keySet(name), numberBook.keySet(number));
+                }
+
+            }
+
+
+            public static void printMap (Map < String, Integer > map){
+                for (String key : map.keySet()) {
+                    System.out.println(key + " => " + map.get(key));
                 }
             }
-
-        public static void printMap(Map<String, String> map) {
-            for (String key : map.keySet()) {
-                System.out.println(key + " => " + map.get(key));
-            }
         }
-
-
     }
 }
+
+
+
