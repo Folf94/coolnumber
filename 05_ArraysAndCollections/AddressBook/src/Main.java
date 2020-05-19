e242d8a0faa434b1794f0c63c00e57d286b38e84
Сафонов Вадим <cska1_94@mail.ru>
2020-05-19 09:37:26 +0300

    lesson 5-4 done

--- 05_ArraysAndCollections/AddressBook/src/Main.java
+++ 05_ArraysAndCollections/AddressBook/src/Main.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Main {
 
-    private static final String MESSAGE_TELEPHONE = "Введите ваш номер телефона";
-    private static final String MESSAGE_NAME = "Введите ваше име";
+    private static final String MESSAGE = "Введите ваше име или номер телефона";
+    private static final String REGEX = "\\d+";
 
     public static void main(String[] args) {
         AddressBook.setNameOrTelephone();
@@ -11,36 +11,39 @@ public class Main {
 
     public static class AddressBook {
         public static void setNameOrTelephone() {
-            Map<String, String> adressBook = new TreeMap<>();
-            Map<String, String> telephoneBook = new TreeMap<>();
+            Map<String, String> addressBook = new TreeMap<>();
             Scanner scanner = new Scanner(System.in);
+            String inputData;
+
             for (; ; ) {
-                System.out.println(MESSAGE_TELEPHONE);
-                String telephoneNumber = scanner.nextLine().trim().replaceAll("[^\\d, LIST]", "");
-                if (telephoneNumber.equals("LIST")) {
-                    printMap(adressBook);
-                    System.out.println(MESSAGE_TELEPHONE);
+                System.out.println(MESSAGE);
+                String number = "";
+                inputData = scanner.nextLine();
+                if (inputData.matches(REGEX)) {
+                    number = inputData.trim();
                 }
-                telephoneBook.put(telephoneNumber, "1");
-                if (telephoneBook.containsKey(telephoneNumber)) {
-                    System.out.println("Данный номер уже зарегестрирован");
-                    System.out.println(MESSAGE_NAME);
-                    String name = scanner.nextLine().toLowerCase().trim().replaceAll("[^a-zA-Zа-яА-я]", "");
-                    telephoneBook.put(telephoneNumber, name);
-                    adressBook.put(name, telephoneNumber);
-                    if (adressBook.containsKey(name)) {
-                        System.out.println("Вы уже зарегестрированы");
-                        System.out.println("Ваше имя: " + name + " Номер телефона: " + telephoneNumber);
-                    } else {
-                        System.out.println(MESSAGE_TELEPHONE);
-                        telephoneNumber = scanner.nextLine().trim().replaceAll("[^\\d]", "");
-                        adressBook.put(telephoneNumber, name);
-                        telephoneBook.put(name, telephoneNumber);
+                if (inputData.equals("LIST")) {
+                    printMap(addressBook);
+                }
+                if (addressBook.containsValue(number)){
+                    System.out.println("Ваш номер телефона уже зарегестрирован, введите имя");
+                    inputData = scanner.nextLine().trim().replaceAll("[^a-zA-Zа-яА-я]", "");
+                    if (addressBook.containsKey(inputData)){
+                        System.out.println("Вы уже зарегестрированы\n");
+                        System.out.println("Ваше имя: " + inputData + " Номер телефона: " + number);
                     }
                 }
+                if (addressBook.containsKey(inputData)) {
+                    System.out.println("Данное имя уже зарегестрировано, введите номер телефона");
+                    number = scanner.nextLine().trim().replaceAll("[^\\d]", "");
+                    if (addressBook.containsValue(number)) {
+                        System.out.println("Вы уже зарегестрированы\n");
+                        System.out.println("Ваше имя: " + inputData + " Номер телефона: " + number);
+                    }
+                }
+                addressBook.put(inputData, number);
             }
         }
-
         public static void printMap(Map<String, String> addressBook) {
             for (String s : addressBook.keySet()) {
                 System.out.println("Ваше имя: " + s + " Номер телефона: " + addressBook.get(s));
