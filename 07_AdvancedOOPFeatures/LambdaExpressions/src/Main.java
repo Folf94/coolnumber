609dbd7122aa38179ec396169aee9ffb3e350119
Сафонов Вадим <cska1_94@mail.ru>
2020-06-11 11:37:48 +0300

    lesson 7-8 done

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -9,8 +9,18 @@ public class Main {
     private static String staffFile = "data/staff.txt";
     private static String dateFormat = "dd.MM.yyyy";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         ArrayList<Employee> staff = loadStaffFromFile();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        Date startDate = formatter.parse("01.01.2017");
+        Date endDate = formatter.parse("31.12.2017");
+
+        Stream<Employee> stream = staff.stream();
+        stream.filter(employee -> employee.getWorkStart().after(startDate)).filter(employee ->
+                employee.getWorkStart().before(endDate)).max(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
+
+        /*
         staff.sort((e1, e2) -> {
             int numberSorting = e1.getSalary().compareTo(e2.getSalary());
             if(numberSorting != 0){
@@ -20,7 +30,7 @@ public class Main {
             return e1.getName().compareTo(e2.getName());
         });
 
-        /*
+
         staff.sort((o1, o2) -> {
             if (o1.getSalary().compareTo(o2.getSalary()) == 0) {
                 o1.getName().compareTo(o2.getName());
@@ -29,12 +39,14 @@ public class Main {
             return o1.getSalary().compareTo(o2.getSalary());
         });
 
-         */
+
 
 
         for (Employee employee : staff) {
             System.out.println(employee);
         }
+         */
+
     }
 
     private static ArrayList<Employee> loadStaffFromFile() {
@@ -47,11 +59,8 @@ public class Main {
                     System.out.println("Wrong line: " + line);
                     continue;
                 }
-                staff.add(new Employee(
-                        fragments[0],
-                        Integer.parseInt(fragments[1]),
-                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
-                ));
+                staff.add(new Employee(fragments[0], Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])));
             }
         } catch (Exception ex) {
             ex.printStackTrace();
