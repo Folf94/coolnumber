c53ad79b5b39ea628b34df019f1c68b707108bc1
Сафонов Вадим <cska1_94@mail.ru>
2020-06-17 14:10:51 +0300

    lesson 7-8  done

--- 07_AdvancedOOPFeatures/Airport/src/Main.java
+++ 07_AdvancedOOPFeatures/Airport/src/Main.java
@@ -1,4 +1,5 @@
 import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
 import com.skillbox.airport.Terminal;
 
 import java.util.Calendar;
@@ -9,12 +10,16 @@ public class Main {
         Airport airport = Airport.getInstance();
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.HOUR, 2);
-        airport.getTerminals().stream().map(Terminal::getFlights).flatMap(Collection::stream).filter(flight -> {
+        airport.getTerminals().stream()
+                .map(Terminal::getFlights)
+                .flatMap(Collection::stream)
+                .filter(flight -> {
             Calendar date = Calendar.getInstance();
             Calendar prasentTime = Calendar.getInstance();
             date.setTime(flight.getDate());
-            return date.before(calendar) && date.after(prasentTime);
-        }).forEach(flight -> System.out.println(flight.getDate()));
+            return date.before(calendar) && date.after(prasentTime) &&
+                    flight.getType().equals(Flight.Type.DEPARTURE); })
+                .forEach(flight -> System.out.println(flight.getDate()));
     }
 
 }
