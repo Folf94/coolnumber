3b82f321dad29f4633c7efa18fefd22378814885
Сафонов Вадим <cska1_94@mail.ru>
2020-08-03 10:39:24 +0300

    lesson 10.3.2 done

--- 10_SQLAndHibernate/Hibernate-exp/src/main/java/Course.java
+++ 10_SQLAndHibernate/Hibernate-exp/src/main/java/Course.java
@@ -1,12 +1,14 @@
+import lombok.Data;
+
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "Courses")
+@Data
 public class Course {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
 
     private String name;
 
@@ -18,112 +20,15 @@ public class Course {
 
     private String description;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "teacher_id")
-    private Teacher teacher;
+    @Column(name = "teacher_id")
+    private int teacherId;
 
-    @Column(name = "students_count", nullable = true)
-    private Integer studentsCount;
+    @Column(name = "students_count")
+    private int studentsCount;
 
     private int price;
 
     @Column(name = "price_per_hour")
     private float pricePerHour;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinTable(name = "Subscriptions", joinColumns = {@JoinColumn (name= "course_id")},
-            inverseJoinColumns = {@JoinColumn (name = "student_id")})
-    private List<Student> studentList;
-
-    @OneToMany(mappedBy = "course")
-    private List<Subscription> subscriptions;
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public CourseType getType() {
-        return type;
-    }
-
-    public void setType(CourseType type) {
-        this.type = type;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
-    public int getStudentsCount() {
-        return studentsCount;
-    }
-
-    public void setStudentsCount(int studentsCount) {
-        this.studentsCount = studentsCount;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public float getPricePerHour() {
-        return pricePerHour;
-    }
-
-    public void setPricePerHour(float pricePerHour) {
-        this.pricePerHour = pricePerHour;
-    }
-
-    public List<Student> getStudentList() {
-        return studentList;
-    }
-
-    public void setStudentList(List<Student> studentList) {
-        this.studentList = studentList;
-    }
-
-    public List<Subscription> getSubscriptions() {
-        return subscriptions;
-    }
-
-    public void setSubscriptions(List<Subscription> subscriptions) {
-        this.subscriptions = subscriptions;
-    }
 }
