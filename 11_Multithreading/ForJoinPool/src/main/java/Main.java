657250039ca57a085d5e2e590b95d35588350a63
Сафонов Вадим <cska1_94@mail.ru>
2020-09-16 16:46:02 +0300

    lesson 11.21 v2

--- 11_Multithreading/ForJoinPool/src/main/java/Main.java
+++ 11_Multithreading/ForJoinPool/src/main/java/Main.java
@@ -1,26 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
-    private static final String URL = "http://www.lenta.ru/";
+
     private static final int processorCoreCount = Runtime.getRuntime().availableProcessors();
     public static void main(String[] args) {
+        String url = "https://www.lenta.ru";
         long start = System.currentTimeMillis();
-        Parser parser = new Parser(URL);
-        String siteMap = processorCoreCount == 0 ? new ForkJoinPool().invoke(parser)
-                : new ForkJoinPool(processorCoreCount).invoke(parser);
+        ConcurrentSkipListSet<String> siteMap = new ConcurrentSkipListSet<>();
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        forkJoinPool.invoke(new Parser(url, url, siteMap));
 
         System.out.println("Scanning end, time = " + ((System.currentTimeMillis() - start) / 1000) + " sec");
         writeFiles(siteMap);
     }
-    private static void writeFiles(String map) {
+    private static void writeFiles(ConcurrentSkipListSet<String> map) {
         String filePath = "Map.txt";
         File file = new File(filePath);
         try (PrintWriter writer = new PrintWriter(file)) {
-            writer.write(map);
+            writer.write(String.valueOf(map));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
--- 11_Multithreading/ForJoinPool/src/main/java/Parser.java
+++ 11_Multithreading/ForJoinPool/src/main/java/Parser.java
@@ -3,15 +3,19 @@
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.RecursiveTask;
 
@@ -19,10 +23,15 @@ public class Parser  extends RecursiveTask<String> {
     @Getter
     @Setter
     private String url;
-    private static CopyOnWriteArraySet<String> links = new CopyOnWriteArraySet<>();
-
+    private static String startUrl;
+    private ConcurrentSkipListSet<String> links = new ConcurrentSkipListSet<>();
     public Parser(String url) {
         this.url = url.trim();
+    }
+    public Parser(String url, String startUrl, ConcurrentSkipListSet<String> links) {
+        this.url = url.trim();
+        Parser.startUrl = startUrl.trim();
+        this.links = links;
     }
 
     @SneakyThrows
@@ -37,24 +46,30 @@ public class Parser  extends RecursiveTask<String> {
         return stringBuffer.toString();
     }
 
-    private void getChildren(Set<Parser> subTask) throws IOException {
+    private void getChildren(Set<Parser> subTask)  {
         Document doc;
         Elements elements;
+
         try {
             Thread.sleep(150);
             doc = Jsoup.connect(url).get();
             elements = doc.select("a");
             for (Element element : elements) {
                 String attr = element.attr("abs:href");
-                if (!attr.isEmpty() && attr.startsWith(url) && !attr
-                        .contains("#")){
+                URL u1 = new URL(url);
+                URL u2 = new URL(attr);
+                if (!attr.isEmpty() && !attr.contains("#")  && !attr.contains("?")
+                        && !attr.contains(".pdf") && u1.getHost().equals(u2.getHost()) && (attr.startsWith("/") || attr.startsWith(startUrl))
+                        && !attr.equals("/")){
                     Parser parser = new Parser(attr);
                     parser.fork();
                     subTask.add(parser);
                     links.add(attr);
                 }
             }
-        } catch (InterruptedException e) {
+        } catch (HttpStatusException | UnsupportedMimeTypeException | MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
