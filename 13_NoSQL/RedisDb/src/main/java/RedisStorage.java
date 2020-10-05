6e2d661fccb74b159ecf875e721cad672d12c17b
Сафонов Вадим <cska1_94@mail.ru>
2020-10-05 13:23:44 +0300

    lesson 13.2 done

---
+++ 13_NoSQL/RedisDb/src/main/java/RedisStorage.java
@@ -0,0 +1,58 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.ScanResult;
+import redis.clients.jedis.Tuple;
+
+import java.util.Date;
+import java.util.List;
+
+public class RedisStorage {
+    List<Tuple> result;
+    Jedis jedis = new Jedis();
+
+    public static void main(String[] args) {
+
+        RedisStorage redisStorage = new RedisStorage();
+        redisStorage.start();
+
+    }
+    private void start(){
+        init();
+        print();
+    }
+
+    private void init() {
+
+        for (int i = 1; i <= 20; i++) {
+            jedis.zadd("Users", new Date().getTime(), String.valueOf(i));
+        }
+
+        ScanResult<Tuple> users = jedis.zscan("Users", "0");
+        result = users.getResult();
+    }
+
+    private void print() {
+
+        for (Tuple user : result) {
+            System.out.println("На главной странице показываем пользователя " + user.getElement());
+
+            try {
+                Thread.sleep(1000);
+                if (Math.random() < 0.1) {
+                    bill();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        print();
+    }
+
+    private void bill() throws InterruptedException {
+
+        int i = (int) (0 + result.size() * Math.random());
+        String element = result.get(i).getElement();
+        System.out.println("Пользователь " + element + " оплатил платную услугу");
+        System.out.println("На главной странице показываем пользователя " + element);
+        Thread.sleep(1000);
+    }
+}
